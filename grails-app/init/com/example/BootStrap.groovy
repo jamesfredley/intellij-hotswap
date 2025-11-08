8ab3d7a57dae5ff07236d5ff3d2c0b62536c1f7b
James Fredley <jamesfredley@users.noreply.github.com>
2025-11-07 19:21:24 -0500

    Print JDK Vendor and Version

--- grails-app/init/com/example/BootStrap.groovy
+++ grails-app/init/com/example/BootStrap.groovy
@@ -3,6 +3,7 @@ package com.example
 class BootStrap {
 
     def init = {
+        System.out.println(System.getProperty("java.vendor") + " " + System.getProperty("java.version"));
     }
 
     def destroy = {
